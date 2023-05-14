9c0db887e78d1743fd22f26fda8f1e551f951e47
Carolina Mattos <mattos.carolina98@gmail.com>
2023-05-14 11:48:20 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#documento_cpf').mask('000.000.000-00', {
+        placeholder:'___.___.___-__'
+    })
+    $('#telefone').mask('(00)00000-0000', {
+        placeholder:'(__)_____-____'
+    })
+    $('#cep').mask('00000-000'), {
+        placeholder:'_____-___'
+    }
+})
